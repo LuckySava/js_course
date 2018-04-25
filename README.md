6fb4973efeb394ac995bfb4d14033c487b4abe36
LuckySava <vivatukraine@yahoo.com>
2018-04-25 10:30:42 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Hi!
+
+Here is my practice in js coding.
+
+Feel free to use it ))))
---
+++ lesson_9/index.js
@@ -0,0 +1,8 @@
+console.log("Савченко Олександр Анатолійович");
+
+// >>>>>>>>>>>>> Another variant <<<<<<<<<<
+
+var name = "Sasha";
+var surName = "Savchenko";
+
+console.log(name + surName);
