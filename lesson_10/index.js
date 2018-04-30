9bd8ea45dd431c828598c7254d1d9d6a978304ce
LuckySava <vivatukraine@yahoo.com>
2018-04-30 12:30:10 +0300

    some improvments

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -1,12 +1,14 @@
-var myAge = 21;
+var myAge = 12;
 var age = myAge % 10;
 
-if (age == 1) {
+if ((myAge % 100) >=10 && (myAge % 100) <= 14) {
+console.log(`Мой возраст ${myAge} лет`);
+} else if (age == 1) {
 console.log(`Мой возраст ${myAge} год`);
 } else if (age == 2 || age == 3 || age == 4) {
 console.log(`Мой возраст ${myAge} года`);
 } else if (age == 5 || age == 6 || age == 7 || age == 8 || age == 9 || age == 0) {
 console.log(`Мой возраст ${myAge} лет`);
-} else {
+}  else {
 console.log("Мой возраст 21 год");
 };
