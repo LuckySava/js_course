719e165efc5a92e580e45918c07b21416b28cf17
LuckySava <vivatukraine@yahoo.com>
2018-06-14 20:49:23 +0300

    fini To Do

--- lesson_22 TO DO LIST/index.js
+++ lesson_22 TO DO LIST/index.js
@@ -1,7 +1,7 @@
 let btn = document.querySelector("#todo__btn");
     // close = document.querySelector("ul li span");
     // li = document.createElement("li");
-    // liAll = document.querySelectorAll("li");
+    liAll = document.querySelectorAll("li");
 
 
 
@@ -9,8 +9,10 @@ btn.addEventListener ("click", () => {
     let input = document.querySelector("#todo__field"),
         inputValue = input.value,
         ul = document.querySelector("#todo__items"),
-        li = document.createElement("li"),
+        li = document.createElement("li");
         liAll = document.querySelectorAll("li");
+        
+    localStorage.setItem("task", input.value)
 
     if (inputValue != "" && inputValue.length <= 100) {
         ul.appendChild(li)
@@ -29,14 +31,18 @@ btn.addEventListener ("click", () => {
 
 // Удаляем элемент li из списка
 
-    let close = document.querySelectorAll("ul li span");
-        
-        for(let i = 0; i < liAll.length; i++) {
+    li.querySelector("span").addEventListener("click", () => {
+        li.classList.add("todo__hide")
+    })
 
-            for(let j = 0; j < close.length; j++) {
-                close[j].addEventListener("click", function() {
-                    li.classList.add("todo__hide");
-                })
-            }
-        }
+    // let close = document.querySelectorAll("ul li span");
+        
+    //     for(let i = 0; i < liAll.length; i++) {
+
+    //         for(let j = 0; j < close.length; j++) {
+    //             close[j].addEventListener("click", function() {
+    //                 li.classList.add("todo__hide");
+    //             })
+    //         }
+    //     }
 });
